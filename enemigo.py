661ec2ccbce653acf20251697f156b3665423bb5
Esteven2003 <bey.viracocha@yavirac.edu.ec>
2026-07-07 09:56:22 -0500

    feat: crear clase Enemigo y metodo para recibir danio

--- enemigo.py
+++ enemigo.py
@@ -1,1 +1,13 @@
 # Código del enemigo que será implementado por Brayan
+class Enemigo:
+    def __init__(self, nombre, vida):
+        self.nombre = nombre
+        self.vida = vida
+
+    def recibir_danio(self, cantidad):
+        self.vida -= cantidad
+        if self.vida <= 0:
+            self.vida = 0
+            print(f"💀 ¡{self.nombre} ha sido derrotado!")
+        else:
+            print(f"🩸 A {self.nombre} le quedan {self.vida} HP.")
